87c4ea48027de009e73c415c6ba84c4bbb08c843
AndreaNoguera08 <93401241+AndreaNoguera08@users.noreply.github.com>
2021-11-02 21:52:16 -0400

    Add files via upload

---
+++ Circunferencia.py
@@ -0,0 +1,5 @@
+radio = float (input("ingrese el radio"))
+longitud = (2*radio)*3.14
+area = (radio**2)*3.14
+print ("la longitud de la circunferencia es : " + str(longitud))
+print ("el area de la circunferencia es: "+ str(area))
