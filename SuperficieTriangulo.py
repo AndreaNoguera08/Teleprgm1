040cdc3b148ef47211f1ebc0668e0a87330f7f74
AndreaNoguera08 <93401241+AndreaNoguera08@users.noreply.github.com>
2021-11-02 21:09:32 -0400

    Add files via upload

---
+++ SuperficieTriangulo.py
@@ -0,0 +1,5 @@
+base = float (input("Ingrese la base del triangulo"))
+altura = float (input("Ingrse la altura del triangulo"))
+Sresultado = (base * altura)/2
+
+print("La superficie del triangulo es: " + str(Sresultado)) 
